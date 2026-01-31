9f0ebec85ad2aec8f2274c2e50dafb43c32207c0
AlexdanTB <alexdanieltb220@gmail.com>
2026-01-31 08:22:41 -0500

    Componente Servicios + Interpolación

---
+++ src/app/shared/servicios/servicios.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servicios',
+  imports: [],
+  templateUrl: './servicios.html',
+  styleUrl: './servicios.css',
+})
+export class Servicios {
+
+  subtitulo = "Cuidamos a los que más qiueres con servicios de alta calidad"
+}
