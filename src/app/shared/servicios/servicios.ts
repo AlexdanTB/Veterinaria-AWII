b597230be61c16ea8545d0a9b7234fb5cff0dc2a
AlexdanTB <alexdanieltb220@gmail.com>
2026-01-31 09:03:31 -0500

    Property binding, servicios

--- src/app/shared/servicios/servicios.ts
+++ src/app/shared/servicios/servicios.ts
@@ -8,5 +8,39 @@ import { Component } from '@angular/core';
 })
 export class Servicios {
 
-  subtitulo = "Cuidamos a los que más qiueres con servicios de alta calidad"
+  subtitulo = "Cuidamos a los que más qiueres con servicios de alta calidad";
+
+  servicioSeleccionado= ""
+
+  //Arreglo de servicios
+  servicios = [
+    {
+    id: 1,
+    nombre: "Consulta General",
+    descripcion: "Evalucación completa del paciente",
+    imagen: "https://purina.com.ec/sites/default/files/2022-10/purina-consulta-veterinaria-para-mascotas-lo-que-debes-saber.jpg",
+    activo: true
+  },
+  {    
+    id: 2,
+    nombre: "Peluquería",
+    descripcion: "El mejor estilo para tu mascota",
+    imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSAo4tEvYQWU517Njpy-0NRnY_sieYKaIReA&s",
+    activo: true
+  },
+  {    
+    id: 3,
+    nombre: "Emergencias",
+    descripcion: "Servicio de ambulancía al instante",
+    imagen: "https://s2.ppllstatics.com/lasprovincias/www/multimedia/201808/11/media/cortadas/127892335--624x350.jpg",
+    activo: false
+  }
+]
+
+//Arreglo para filtrar los servicios
+serviciosFiltrados = this.servicios;
+  seleccionar(nombre: string){
+    this.servicioSeleccionado= nombre; 
+  }
+
 }
