38bdfeca4d2a8b701a30933771acd3dee60b0c73
AlexdanTB <alexdanieltb220@gmail.com>
2026-01-31 09:35:11 -0500

    Enrutamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './features/home-page/home-page';
+import { NosotrosPage } from './features/nosotros-page/nosotros-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //Ruta inicial
+    {path:'',component: HomePage},
+    {path:'nosotros', component:NosotrosPage}
+
+    //Ruta error 404
+    //{path:'**', component:Pagina404}
+];
---
+++ src/app/features/nosotros-page/nosotros-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nosotros-page',
+  imports: [],
+  templateUrl: './nosotros-page.html',
+  styleUrl: './nosotros-page.css',
+})
+export class NosotrosPage {
+
+}
