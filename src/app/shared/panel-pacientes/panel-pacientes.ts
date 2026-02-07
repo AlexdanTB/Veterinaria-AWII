46bd7682341569eaa04c526966dd24ead840933e
AlexdanTB <alexdanieltb220@gmail.com>
2026-02-07 08:20:07 -0500

    ng model, style, class

--- src/app/shared/panel-pacientes/panel-pacientes.ts
+++ src/app/shared/panel-pacientes/panel-pacientes.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-panel-pacientes',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './panel-pacientes.html',
   styleUrl: './panel-pacientes.css',
 })
 export class PanelPacientes {
+  nombreFiltro: string = "";
+
+  pacientes = [
+    {nombre: 'Ramón', especie: 'Perro', urgencia: 'alta', recuperacion:85},
+    {nombre: 'Raquel', especie: 'Gato', urgencia: 'media', recuperacion:40},
+    {nombre: 'Rafael', especie: 'Tortuga', urgencia: 'baja', recuperacion:10},
+    {nombre: 'Nacho', especie: 'Gallo', urgencia: 'alta', recuperacion:90}
+  ]
+    
 
 }
