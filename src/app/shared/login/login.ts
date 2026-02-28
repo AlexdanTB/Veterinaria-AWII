61c99c88d2916301128ec2fea0ee720a4f4e72d0
AlexdanTB <alexdanieltb220@gmail.com>
2026-02-28 09:13:04 -0500

    feat: login autenticación

--- src/app/shared/login/login.ts
+++ src/app/shared/login/login.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgClass } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
+import { AuthService } from '../../services/auth-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+
+  email: string = ''
+  password: string = ''
+
+  private servicioAuth = inject(AuthService);
+  private router = inject(Router)
+
+  iniciarSesion(){
+    if(this.email! && this.password!){
+      
+    }
+    this.servicioAuth.login(this.email, this.password);
+    alert('Bienvenido al sistema');
+    this.router.navigate(['usuarios'])  
+    
+  }
 
 }
