48e75c98de2f1f99eed53c73e13f4cb888f16adc
AlexdanTB <alexdanieltb220@gmail.com>
2026-02-28 08:07:40 -0500

    feat: configuraciones firebase

---
+++ src/app/config/firebase.config.ts
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCPQAhK3TQu36OrXqoD4qUGCdT71f_W2a4",
+  authDomain: "veterinaria-aw.firebaseapp.com",
+  databaseURL: "https://veterinaria-aw-default-rtdb.firebaseio.com",
+  projectId: "veterinaria-aw",
+  storageBucket: "veterinaria-aw.firebasestorage.app",
+  messagingSenderId: "480678807010",
+  appId: "1:480678807010:web:86aea7dd4bc6ef874fe5bb"
+};
